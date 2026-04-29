9a91cb62016a0386d64c4e68900a3aeb0b7aefb1
Darius Valancauskas <6602631+darius-v@users.noreply.github.com>
2026-04-29 20:05:56 +0300

    FileControllerFactory created

--- public/index.php
+++ public/index.php
@@ -2,18 +2,6 @@
 
 require __DIR__ . '/../autoload.php';
 
-use App\Controller\FileController;
-use App\Factory\ParserFactory;
-use App\Http\Request;
-use App\Parser\ParserDiscovery;
-use App\Service\FileUploadService;
-use App\Validator\FileValidator;
+use App\Factory\FileControllerFactory;
 
-$discovery = new ParserDiscovery(__DIR__ . '/../src/Parser', 'App\\Parser');
-$factory   = new ParserFactory($discovery);
-$validator = new FileValidator($factory);
-$service   = new FileUploadService($validator, $factory);
-$request   = Request::fromGlobals();
-
-$controller = new FileController($service, $request);
-$controller->index();
+FileControllerFactory::create()->index();
---
+++ src/Factory/FileControllerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Factory;
+
+use App\Controller\FileController;
+use App\Http\Request;
+use App\Parser\ParserDiscovery;
+use App\Service\FileUploadService;
+use App\Validator\FileValidator;
+
+/**
+ * Wires up and returns a fully configured FileController.
+ */
+class FileControllerFactory
+{
+    /**
+     * @return FileController
+     */
+    public static function create(): FileController
+    {
+        $discovery = new ParserDiscovery(__DIR__ . '/../Parser', 'App\\Parser');
+        $factory   = new ParserFactory($discovery);
+        $validator = new FileValidator($factory);
+        $service   = new FileUploadService($validator, $factory);
+        $request   = Request::fromGlobals();
+
+        return new FileController($service, $request);
+    }
+}
