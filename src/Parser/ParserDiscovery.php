e190f905ebe68730b940097c11c4a26b8e740fed
Darius Valancauskas <6602631+darius-v@users.noreply.github.com>
2026-04-29 23:33:46 +0300

    Removed dead code

--- src/Parser/ParserDiscovery.php
+++ src/Parser/ParserDiscovery.php
@@ -14,14 +14,9 @@ class ParserDiscovery
      * Returns FQCNs of all instantiable classes implementing ParserInterface.
      *
      * @return array<int, class-string<ParserInterface>>
-     * @throws \RuntimeException If the directory cannot be read.
      */
     public function discover(): array
     {
-        if (!is_dir(self::DIRECTORY) || !is_readable(self::DIRECTORY)) {
-            throw new \RuntimeException('Parser directory not readable: ' . self::DIRECTORY);
-        }
-
         $found = [];
 
         foreach (glob(self::DIRECTORY . '/*.php') as $file) {
